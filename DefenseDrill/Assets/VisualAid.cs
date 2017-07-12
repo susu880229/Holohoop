d1dee3a7f7d28afdac4f432854c8cd7e95918514
YuyuG <guanyouhan92@gmail.com>
2017-07-12 13:27:38 -0700

    Cone integration

---
+++ DefenseDrill/Assets/VisualAid.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VisualAid : MonoBehaviour {
+    private bool va = false;
+    private bool tempVA;
+    private int vac = 0;
+
+    private Renderer[] rd;
+
+	// Use this for initialization
+	void Start () {
+        tempVA = va;
+        rd = this.GetComponentsInChildren<Renderer>();
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.V))
+        {
+            va = !va;
+            if (tempVA != va)
+            {
+                foreach (Renderer r in rd) {
+                    r.enabled = va;
+                }
+
+                tempVA = va;
+            }
+        }
+	}
+
+    void OnVisualAid()
+    {
+        va = !va;
+        if (tempVA != va)
+        {
+            foreach (Renderer r in rd)
+            {
+                r.enabled = va;
+            }
+
+            tempVA = va;
+        }
+    }
+
+    
+}
