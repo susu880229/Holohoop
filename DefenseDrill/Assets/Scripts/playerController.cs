6a69b0e3f47cfffeb88d0ecf0bb6821a77167600
DESKTOP-BVH5CHT\susu880229 <susu880229@gmail.com>
2017-07-12 15:47:06 -0700

    solve the color dont' change issue when player restart

--- DefenseDrill/Assets/Scripts/playerController.cs
+++ DefenseDrill/Assets/Scripts/playerController.cs
@@ -58,21 +58,11 @@ public class playerController : MonoBehaviour
         if (other.gameObject.name == "trigger" + ball_script.to_index)
         {
 			bIsPlayerInTrigger = true;
-			//changing the trigger to green
+           
             //other.gameObject.GetComponent<Renderer>().material.color = zone_green;
-			//changign the shirt of the attacker from grey --> green;
-			if (ball_script.to_index == 0) {
-				player0.gameObject.GetComponent<Renderer> ().material.color = zone_green;
-			} else if (ball_script.to_index == 1) {
-				player1.gameObject.GetComponent<Renderer> ().material.color = zone_green;
-                if (!ball_script.StartPlay)
-                {
-                    TimerUI.GetComponent<CanvasGroup>().alpha = 1f;
-                    TriggerRenderer.enabled = false;
-                }
-			} else if (ball_script.to_index == 2) {
-				player2.gameObject.GetComponent<Renderer> ().material.color = zone_green;
-			}
+
+            //changign the shirt of the attacker from grey --> green;
+            player_Green();
 
             if (!ball_script.StartPlay)
             {
@@ -101,6 +91,8 @@ public class playerController : MonoBehaviour
     {
         if (other.gameObject.name == "trigger" + ball_script.to_index)
         {
+            //to avoid sometimes it didn't turn green after restart and player stand in the middle
+            player_Green();
 
             if (pre != cur)
             {
@@ -147,7 +139,29 @@ public class playerController : MonoBehaviour
         cur = -1;
     }
 
-	public bool getIsPlayerInTrigger(){
+	public bool getIsPlayerInTrigger()
+    {
 		return bIsPlayerInTrigger;
 	}
+
+    void player_Green()
+    {
+        if (ball_script.to_index == 0)
+        {
+            player0.gameObject.GetComponent<Renderer>().material.color = zone_green;
+        }
+        else if (ball_script.to_index == 1)
+        {
+            player1.gameObject.GetComponent<Renderer>().material.color = zone_green;
+            if (!ball_script.StartPlay)
+                {
+                    TimerUI.GetComponent<CanvasGroup>().alpha = 1f;
+                    TriggerRenderer.enabled = false;
+                }
+        }
+        else if (ball_script.to_index == 2)
+        {
+            player2.gameObject.GetComponent<Renderer>().material.color = zone_green;
+        }
+    }
 }
